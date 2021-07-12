17ab87be388f9c3ba68bf1d51946e746f7b3c42e
Henry Gillard <henry@MacBook-Pro.local>
2021-07-12 14:14:41 -0700

    Part 1 complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Drummer:
+    def __init__(drummer, name, band, description, age, sponsor):
+        drummer.name = name
+        drummer.band = band
+        drummer.description = description
+        drummer.age = age
+        drummer.sponsor = sponsor
+
+drummers = [
+    Drummer("Chris Coleman", "Beck", "Mainly a solo artist, Chris also plays for Beck.", 38, "Meinl"),
+    Drummer("Tommy Igoe", "Tommy Igoe Big Band", "Tommy is the creator the Groove Essentials, wrote the drum book for the Lion King musical, and has over 60,000 followers on social media", 56, "Vic Firth"),
+    Drummer("Niel Peart", "Rush", "Niel Peart is considered a drumming legend, may he RIP", 67, "Remo"),
+]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import drummers as drummers
 # Create your views here.
 def home(request):
     return HttpResponse("<h1>Hello! And welcome to Drummers Collector!</h1>")
 
 def about(request):
     return render(request, "about.html")
+
+def drummers_index(request):
+    return render(request, "drummers/index.html", { "drummers": drummers})
     
